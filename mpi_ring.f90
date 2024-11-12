574fd8d0531f81e41e6f345d10bb46aaa323a9fb
Alex Buccheri <alexanderbuccheri@googlemail.com>
2024-11-12 20:27:52 +0000

    Clean up the code and abstract generation of the ring pattern array to a routine

--- mpi_ring.f90
+++ mpi_ring.f90
@@ -14,66 +14,44 @@
 
 
 ! mpif90 -pedantic -fbacktrace -fbounds-check -Wl,-syslibroot,/Library/Developer/CommandLineTools/SDKs/MacOSX.sdk mpi_ring.f90 -o mpi_ring
-! mpirun -np 2 ./mpi_ring
+! mpirun -np 3 ./mpi_ring
 module mpi_routines
   implicit none
   public
 
-
 contains
 
-  !> @brief Number of elements on process 'rank' for an array of n elements,
-  !! distributed over np processes.
-  integer function distribute_elements(np, n, rank)
-    integer, intent(in) :: n               !< Number of elements to distribute
-    integer, intent(in) :: np              !< Number of processes to distribute across
-    integer, intent(in) :: rank            !< Process to compute local number of elements
-    integer, allocatable :: indices(:, :)  !< Start and stop indices for each each process
-    indices = distribute_elements_start_stop(np, n)
-    distribute_elements = indices(2, rank + 1) - indices(1, rank + 1) + 1
-  end function distribute_elements
-
-  !> @brief Distribute N elements over NP processes.
+  !> @brief Produce an index array of receive-from and send-to ranks per iteration of a ring pattern.
   !!
-  !! Example 1: n = 12 and np = 3
-  !! [1, 2, 3, 4 | 5, 6, 7, 8 | 9, 10, 11, 12]
-  !!
-  !! Example 2: n = 14 and np = 4
-  !! If n is not integer-divisible by np, assign the remainders equally between the first n_remainder processes
-  !! [1, 2, 3, 4 | 5, 6, 7, 8 |9, 10, 11 |12, 13, 14]
-  !!
-  function distribute_elements_start_stop(np, n) result(indices)
-    integer, intent(in) :: n       !< Number of elements to distribute
-    integer, intent(in) :: np      !< Number of processes to distribute across
-    integer :: elements_per_process, n_remainder
-    integer :: n_assigned_processes, n_assigned_elements
-    integer :: iprocess, istart, istop
-    integer :: indices(2, np)
+  !! \p receive_send has ranks defined for the local process \p rank.
+  subroutine mpi_ring_pattern_receive_send_ranks(rank, np, receive_send)
+    integer,              intent(in ) :: rank               !< MPI rank
+    integer,              intent(in ) :: np                 !< Number of MPI processes
+    integer, allocatable, intent(out) :: receive_send(:, :) !<  receive-from and send-to ranks
+    
+    integer, allocatable :: initial_receive_from(:), initial_send_to(:),  receive_from(:), send_to(:)
+    integer              :: nring, iround, i
 
-    ! When load-balanced
-    elements_per_process = int(n / np)
-    n_remainder = mod(n, np)
+    !  Number of ring pattern iterations
+    nring = np - 1 
+    
+    ! Start send-tos and receive-froms by permuting this process's rank
+    initial_send_to = modulo([(i, i = 0, np-1)] + 1, np)
+    initial_receive_from = modulo([(i, i = 0, np-1)] - 1, np)
+    
+    ! Defines receive-from and send-to ranks for each round of the ring pattern
+    ! Defined specifically for this local MPI process
+    allocate(receive_send(2, nring))
+    
+    ! Round comm pattern
+    do iround = 1, nring
+       send_to = modulo(initial_send_to + iround - 1, np)
+       receive_from = modulo(initial_receive_from - (iround - 1), np)
+       receive_send(:, iround) = [receive_from(rank+1), send_to(rank+1)]
+    enddo
 
-    ! Assign processes with extra elements first
-    iprocess = 0
-    do while (n_remainder > 0)
-       iprocess = iprocess + 1
-       istart = 1 + (iprocess - 1)*(elements_per_process + 1)
-       istop = iprocess * (elements_per_process + 1)
-       indices(:, iprocess) = [istart, istop]
-       n_remainder = n_remainder - 1
-    end do
-
-    n_assigned_processes = iprocess
-    n_assigned_elements = (elements_per_process + 1) * n_assigned_processes
-
-    do iprocess = n_assigned_processes + 1, np
-       istart = 1 + ((iprocess - n_assigned_processes - 1) * elements_per_process) + n_assigned_elements
-       istop = ((iprocess - n_assigned_processes) * elements_per_process) + n_assigned_elements
-       indices(:, iprocess) = [istart, istop]
-    end do
-
-  end function distribute_elements_start_stop
+  end subroutine mpi_ring_pattern_receive_send_ranks
+  
 
 end module mpi_routines
 
@@ -85,45 +63,23 @@ program mpi_ring
   use  mpi_routines
   implicit none
 
-  integer :: rank, np, ierr, iprocess, send_id, rec_id, iround, send_index
-  integer, allocatable :: collated_data(:), send_to(:), receive_from(:), initial_send_to(:), initial_receive_from(:), receive_send(:, :)
-  integer :: status(MPI_STATUS_SIZE)
-
-  integer :: i, cnt, n_values, n_local, ij
-  integer, allocatable :: local_state_indices(:), indices(:, :), pair_indices(:, :)
-
   integer :: reqs(2), stats(MPI_STATUS_SIZE, 2)
+  integer :: rank, np, ierr
+
+  integer :: iprocess, iround
   integer :: irecv, isend, rf, st, tag
   integer :: send_buffer, recv_buffer
   
+  integer, allocatable :: receive_send(:, :)
+
   call MPI_INIT(ierr)
   call MPI_Comm_rank(MPI_COMM_WORLD, rank, ierr)
   call MPI_Comm_size(MPI_COMM_WORLD, np, ierr)
 
-  ! Start send-tos and receive-froms by permuting this process's rank
-  initial_send_to = modulo([(i, i = 0, np-1)] + 1, np)
-  initial_receive_from = modulo([(i, i = 0, np-1)] - 1, np)
-
-  ! Defines receive-from and send-to ranks for each round of the ring pattern
-  ! Defined specifically for this local MPI process
-  allocate(receive_send(2, np -1))
-  
-  ! Round comm pattern
-  do iround = 1, np - 1
-     send_to = modulo(initial_send_to + iround - 1, np)
-     receive_from = modulo(initial_receive_from - (iround - 1), np)
-     receive_send(:, iround) = [receive_from(rank+1), send_to(rank+1)]
-  enddo
-
-!!$  do iround = 1, np - 1
-!!$     write(*, *) "** Round ", iround, " **"
-!!$     if(rank ==0) write(*, *) 'receive-from, rank, send-to', receive_send(1, iround), rank, receive_send(2, iround)
-!!$     if(rank ==1) write(*, *) 'receive-from, rank, send-to', receive_send(1, iround), rank, receive_send(2, iround)
-!!$     if(rank ==2) write(*, *) 'receive-from, rank, send-to', receive_send(1, iround), rank, receive_send(2, iround)
-!!$  enddo
+  call  mpi_ring_pattern_receive_send_ranks(rank, np, receive_send)
 
   ! Start rounds with non-blocking communication
-  do iround = 1, np - 1
+  do iround = 1, size(receive_send, 2)
      ! Prepare buffers
      send_buffer = rank
      recv_buffer = -1
